dc8dd0449957c65a680db84555f2e7e244d4bf23
Abror Boqiyev <Boqiyevabror@gmail.com>
2024-12-11 17:13:09 +0500

    some design and buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root "pages#home"
   get 'pages/teachers'
   get 'pages/students'
   resources :grades
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#home"
 end
