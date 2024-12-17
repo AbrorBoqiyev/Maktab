a39152913d03c58e5ac27f508b7767cbb4828d15
Abror Boqiyev <Boqiyevabror@gmail.com>
2024-12-17 18:31:23 +0500

    added fullname func to teacher

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,12 @@
 class Teacher < ApplicationRecord
   validates :name, presence: true 
+  validates :surname, presence: true
   has_many :subjects
   has_many :grades
+
+
+  def fullname
+    "#{name} #{surname}"
+  end
+
 end
