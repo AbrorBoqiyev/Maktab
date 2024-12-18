b7b9b1946dc08308afc457fa546e7719bb264d1a
Abror Boqiyev <Boqiyevabror@gmail.com>
2024-12-18 17:43:06 +0500

    added number limit and dependent destroy in studentModel

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
   validates :name, presence: true, length: {minimum: 2}  #name=>firstname
-  # validates :age, presence: true, {5< , 25<}, firstname, lastname, method: fullname
-  has_many :grades
-  belongs_to :subject
+  validates :age, numericality: {only_integer: true}, inclusion: {in: 5..25, message: "must be between 5 and 25"}
+  has_many :grades, dependent: :destroy
+  # belongs_to :subject
 end
