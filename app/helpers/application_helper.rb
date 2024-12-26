8e4d4da274df9e601d2cadc12500a53e6997b157
Abror Boqiyev <Boqiyevabror@gmail.com>
2024-12-26 09:59:38 +0500

    helper toggle_direction function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def toggle_direction(current_direction)
+    current_direction == "asc" ? "desc" : "asc"
+  end
+
+  def arrow(current_sort, column, current_direction)
+    return unless current_sort == column
+    current_direction == "asc" ? "⬇️" : "⬆️"
+  end
 end
